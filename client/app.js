eb99efe41104ba235b94e81e8ef5017214cd271a
shaima96 <141062@ppu.edu.ps>
2020-11-03 15:50:54 +0200

    Never gonna let you down ^_^

--- client/app.js
+++ client/app.js
@@ -2,23 +2,40 @@
 
 var from = document.getElementById('form');
 
-$('#form').on('submit', function(e){
+$('#form').on('submit', function (e) {
     e.preventDefault();
-  });
+});
 
 
 var input = document.getElementById("file");
 var output = document.getElementById("output");
 
 input.addEventListener("change", function () {
-  if (this.files && this.files[0]) {
-    var myFile = this.files[0];
-    var reader = new FileReader();
-    
-    reader.addEventListener('load', function (e) {
-      output.textContent = e.target.result;
+    if (this.files && this.files[0]) {
+        var myFile = this.files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function (e) {
+            output.textContent = e.target.result;
+        });
+
+        reader.readAsBinaryString(myFile);
+    }
+});
+
+var button = document.getElementById("button");
+var value = $('#file').val();
+$('#button').on('click', (e) => {
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        dataType: 'json',
+        body: JSON.stringify({ data: value }),
+        success: function (result) {
+            console.log(result);
+        },
+        error: function (error) {
+            console.log(error);
+        }
     });
-    
-    reader.readAsBinaryString(myFile);
-  }   
 });
--- server.js
+++ server.js
@@ -2,30 +2,32 @@
 const express = require('express');
 const path = require('path');
 const port = 4568;
+var bodyParser = require('body-parser')
 
 const app = express();
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
-    res.end(' show html page ');
-  });
-  
+    res.send(' show html page ');
+});
 
-app.post('/csv', (req, res) =>
-{ 
-    // req.body.json
-    res.send("ggggggggg");
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 
 app.listen(port, () => {
-  console.log(`App is listening on ${port}`);
+    console.log(`App is listening on ${port}`);
 });
 
 

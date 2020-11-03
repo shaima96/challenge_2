7a38f32120f7ee752d3c13b79e8d28fb071e279d
shaima96 <141062@ppu.edu.ps>
2020-11-03 11:55:51 +0200

    Initail files

--- client/app.js
+++ client/app.js
@@ -1,2 +1,22 @@
 
+const express = require('express');
+const path = require('path');
 
+const app = express();
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+  });
+  
+
+app.post('/upload_json', (req, res) =>
+{ 
+
+})
+
+
+  module.exports = app;
--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 
+const app = require('./client/app.js');
+const port = 4568;
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`);
+});
+
+
+
 
